f2279c88a4e567b67dd1558e0f224fc72332d0c6
Szilard Szaloki <szilardszaloki@gmail.com>
2021-06-21 14:12:42 +0200

    Removing anon_transfer_checked completely.

--- vendor/bat-native-ledger/src/bat/ledger/internal/state/state.h
+++ vendor/bat-native-ledger/src/bat/ledger/internal/state/state.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
@@ -106,10 +106,6 @@ class State {
   void SetPromotionLastFetchStamp(const uint64_t stamp);
 
   uint64_t GetPromotionLastFetchStamp();
-
-  void SetAnonTransferChecked(const bool checked);
-
-  bool GetAnonTransferChecked();
 
  private:
   LedgerImpl* ledger_;  // NOT OWNED
