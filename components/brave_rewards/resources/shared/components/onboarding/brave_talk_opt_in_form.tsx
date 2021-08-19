f32bd26cb56f096eba14c71e6935a60d4c3fc010
Aleksey Seren <aseren@brave.com>
2021-08-19 22:42:14 +0700

    Added request ads enabled api browser tests.
    fix https://github.com/brave/brave-browser/issues/16964

--- components/brave_rewards/resources/shared/components/onboarding/brave_talk_opt_in_form.tsx
+++ components/brave_rewards/resources/shared/components/onboarding/brave_talk_opt_in_form.tsx
@@ -69,7 +69,7 @@ export function BraveTalkOptInForm (props: Props) {
           {getString('braveTalkPrivateAdsDescription')}
         </style.text>
         <style.enable>
-          <MainButton onClick={onRewardsTurnedOn}>
+          <MainButton onClick={onRewardsTurnedOn} testId={'brave-talk-turn-on-private-ads'}>
             {getString('braveTalkTurnOnPrivateAds')}
           </MainButton>
         </style.enable>
@@ -87,7 +87,7 @@ export function BraveTalkOptInForm (props: Props) {
         {getString('braveTalkBraveRewardsDescription')}
       </style.text>
       <style.enable>
-        <MainButton onClick={onRewardsTurnedOn}>
+        <MainButton onClick={onRewardsTurnedOn} testId={'brave-talk-turn-on-rewards'}>
           {getString('braveTalkTurnOnRewards')}
         </MainButton>
       </style.enable>
