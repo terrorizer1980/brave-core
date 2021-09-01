8a47e883937c674ee2aa473690d03b27ddad451c
Sergey P <spylogsster@gmail.com>
2021-09-01 11:20:37 +0300

    temp changes

--- components/brave_wallet/common/web3_provider_utils.cc
+++ components/brave_wallet/common/web3_provider_utils.cc
@@ -13,7 +13,7 @@
 
 namespace brave_wallet {
 
-absl::optional<mojom::EthereumChain> ParameterValueToEthereumChain(
+absl::optional<mojom::EthereumChain> ValueToEthereumChain(
     const base::Value& value) {
   mojom::EthereumChain chain;
   const base::DictionaryValue* params_dict = nullptr;
@@ -68,57 +68,6 @@ absl::optional<mojom::EthereumChain> ParameterValueToEthereumChain(
   return chain;
 }
 
-absl::optional<mojom::EthereumChain> ValueToEthereumChain(
-    const base::Value& value) {
-  mojom::EthereumChain chain;
-  const base::DictionaryValue* params_dict = nullptr;
-  if (!value.GetAsDictionary(&params_dict) || !params_dict)
-    return absl::nullopt;
-
-  const std::string* chain_id = params_dict->FindStringKey("chainId");
-  if (!chain_id) {
-    return absl::nullopt;
-  }
-  chain.chain_id = *chain_id;
-
-  const std::string* chain_name = params_dict->FindStringKey("chainName");
-  if (chain_name) {
-    chain.chain_name = *chain_name;
-  }
-
-  const base::Value* explorerUrlsListValue =
-      params_dict->FindListKey("blockExplorerUrls");
-  if (explorerUrlsListValue) {
-    for (const auto& entry : explorerUrlsListValue->GetList())
-      chain.block_explorer_urls.push_back(entry.GetString());
-  }
-
-  const base::Value* iconUrlsValue = params_dict->FindListKey("iconUrls");
-  if (iconUrlsValue) {
-    for (const auto& entry : iconUrlsValue->GetList())
-      chain.icon_urls.push_back(entry.GetString());
-  }
-
-  const base::Value* rpcUrlsValue = params_dict->FindListKey("rpcUrls");
-  if (rpcUrlsValue) {
-    for (const auto& entry : rpcUrlsValue->GetList())
-      chain.rpc_urls.push_back(entry.GetString());
-  }
-  const std::string* symbol_name = params_dict->FindStringKey("name");
-  if (symbol_name) {
-    chain.name = *symbol_name;
-  }
-  const std::string* symbol = params_dict->FindStringKey("symbol");
-  if (symbol) {
-    chain.symbol = *symbol;
-  }
-  absl::optional<int> decimals = params_dict->FindIntKey("decimals");
-  if (decimals) {
-    chain.decimals = decimals.value();
-  }
-  return chain;
-}
-
 base::Value EthereumChainToValue(const mojom::EthereumChainPtr& chain) {
   base::Value dict(base::Value::Type::DICTIONARY);
   dict.SetStringKey("chainId", chain->chain_id);
@@ -145,10 +94,11 @@ base::Value EthereumChainToValue(const mojom::EthereumChainPtr& chain) {
     rpcUrlsValue.AppendString(url);
   }
   dict.SetKey("rpcUrls", std::move(rpcUrlsValue));
-
-  dict.SetStringKey("name", chain->name);
-  dict.SetStringKey("symbol", chain->symbol);
-  dict.SetIntKey("decimals", chain->decimals);
+  base::Value currency(base::Value::Type::DICTIONARY);
+  currency.SetStringKey("name", chain->name);
+  currency.SetStringKey("symbol", chain->symbol);
+  currency.SetIntKey("decimals", chain->decimals);
+  dict.SetKey("nativeCurrency", std::move(currency));
   return dict;
 }
 
