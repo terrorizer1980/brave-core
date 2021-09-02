milahu/vanilla-brave-core

Implement tainted tokens

vendor/bat-native-ads/src/bat/ads/internal/tokens/get_issuers/get_issuers_url_request_builder.cc
@@ -1,0 +1,34 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "bat/ads/internal/tokens/get_issuers/get_issuers_url_request_builder.h"
+
+#include "base/strings/stringprintf.h"
+#include "bat/ads/internal/server/confirmations_server_util.h"
+
+namespace ads {
+
+GetIssuersUrlRequestBuilder::GetIssuersUrlRequestBuilder() = default;
+
+GetIssuersUrlRequestBuilder::~GetIssuersUrlRequestBuilder() = default;
+
+// GET /v1/issuers/
+
+mojom::UrlRequestPtr GetIssuersUrlRequestBuilder::Build() {
+  mojom::UrlRequestPtr url_request = mojom::UrlRequest::New();
+  url_request->url = BuildUrl();
+  url_request->method = mojom::UrlRequestMethod::kGet;
+
+  return url_request;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+std::string GetIssuersUrlRequestBuilder::BuildUrl() const {
+  return base::StringPrintf("%s/v1/issuers/",
+                            confirmations::server::GetHost().c_str());
+}
+
+}  // namespace ads

vendor/bat-native-ads/src/bat/ads/internal/tokens/redeem_unblinded_token/redeem_unblinded_token.h
@@ -6,8 +6,7 @@
 #ifndef BRAVE_VENDOR_BAT_NATIVE_ADS_SRC_BAT_ADS_INTERNAL_TOKENS_REDEEM_UNBLINDED_TOKEN_REDEEM_UNBLINDED_TOKEN_H_
 #define BRAVE_VENDOR_BAT_NATIVE_ADS_SRC_BAT_ADS_INTERNAL_TOKENS_REDEEM_UNBLINDED_TOKEN_REDEEM_UNBLINDED_TOKEN_H_
 
-#include <string>
-
+#include "bat/ads/internal/tokens/get_issuers/get_issuers.h"
 #include "bat/ads/internal/tokens/redeem_unblinded_token/redeem_unblinded_token_delegate.h"
 #include "bat/ads/public/interfaces/ads.mojom.h"
 
@@ -35,6 +34,9 @@ class RedeemUnblindedToken {
   void OnCreateConfirmation(const mojom::UrlResponse& url_response,
                             const ConfirmationInfo& confirmation);
 
+  void RequestIssuers(const ConfirmationInfo& confirmation);
+  void OnRequestIssuers(const ConfirmationInfo& confirmation);
+
   void FetchPaymentToken(const ConfirmationInfo& confirmation);
   void OnFetchPaymentToken(const mojom::UrlResponse& url_response,
                            const ConfirmationInfo& confirmation);
@@ -46,6 +48,7 @@ class RedeemUnblindedToken {
   void OnFailedToRedeemUnblindedToken(const ConfirmationInfo& confirmation,
                                       const bool should_retry);
 
+  std::unique_ptr<GetIssuers> get_issuers_;
   RedeemUnblindedTokenDelegate* delegate_ = nullptr;
 };
 

vendor/bat-native-ads/src/bat/ads/internal/tokens/refill_unblinded_tokens/refill_unblinded_tokens.cc
@@ -21,6 +21,7 @@
 #include "bat/ads/internal/privacy/unblinded_tokens/unblinded_tokens.h"
 #include "bat/ads/internal/server/ads_server_util.h"
 #include "bat/ads/internal/time_formatting_util.h"
+#include "bat/ads/internal/tokens/get_issuers/get_issuers_url_request_builder.h"
 #include "bat/ads/internal/tokens/refill_unblinded_tokens/get_signed_tokens_url_request_builder.h"
 #include "bat/ads/internal/tokens/refill_unblinded_tokens/request_signed_tokens_url_request_builder.h"
 #include "net/http/http_status_code.h"
@@ -44,7 +45,8 @@ const int kMaximumUnblindedTokens = 50;
 
 RefillUnblindedTokens::RefillUnblindedTokens(
     privacy::TokenGeneratorInterface* token_generator)
-    : token_generator_(token_generator) {
+    : get_issuers_(std::make_unique<GetIssuers>()),
+      token_generator_(token_generator) {
   DCHECK(token_generator_);
 }
 
@@ -80,19 +82,18 @@ void RefillUnblindedTokens::MaybeRefill(const WalletInfo& wallet) {
 
   wallet_ = wallet;
 
-  const CatalogIssuersInfo catalog_issuers =
-      ConfirmationsState::Get()->get_catalog_issuers();
-  if (!catalog_issuers.IsValid()) {
-    BLOG(0, "Failed to refill unblinded tokens due to missing catalog issuers");
+  // const CatalogIssuersInfo catalog_issuers =
+  //     ConfirmationsState::Get()->get_catalog_issuers();
+  // if (!catalog_issuers.IsValid()) {
+  //   BLOG(0, "Failed to refill unblinded tokens due to missing catalog
+  //   issuers");
 
-    if (delegate_) {
-      delegate_->OnFailedToRefillUnblindedTokens();
-    }
+  //   if (delegate_) {
+  //     delegate_->OnFailedToRefillUnblindedTokens();
+  //   }
 
-    return;
-  }
-
-  public_key_ = catalog_issuers.public_key;
+  //   return;
+  // }
 
   Refill();
 }
@@ -108,12 +109,26 @@ void RefillUnblindedTokens::Refill() {
 
   nonce_ = "";
 
+  RequestIssuers();
+}
+
+void RefillUnblindedTokens::RequestIssuers() {
+  auto callback = std::bind(&RefillUnblindedTokens::OnRequestIssuers, this);
+  get_issuers_->RequestIssuers(callback);
+}
+
+void RefillUnblindedTokens::OnRequestIssuers() {
+  if (!get_issuers_->IsInitialized()) {
+    OnFailedToRefillUnblindedTokens(/* should_retry */ false);
+    return;
+  }
+
   RequestSignedTokens();
 }
 
 void RefillUnblindedTokens::RequestSignedTokens() {
   BLOG(1, "RequestSignedTokens");
-  BLOG(2, "POST /v1/confirmation/token/{payment_id}");
+  BLOG(2, "POST /v2/confirmation/token/{payment_id}");
 
   const int count = CalculateAmountOfTokensToRefill();
   tokens_ = token_generator_->Generate(count);
@@ -123,7 +138,7 @@ void RefillUnblindedTokens::RequestSignedTokens() {
   RequestSignedTokensUrlRequestBuilder url_request_builder(wallet_,
                                                            blinded_tokens_);
   mojom::UrlRequestPtr url_request = url_request_builder.Build();
-  BLOG(5, UrlRequestToString(url_request));
+  BLOG(6, UrlRequestToString(url_request));
   BLOG(7, UrlRequestHeadersToString(url_request));
 
   auto callback = std::bind(&RefillUnblindedTokens::OnRequestSignedTokens, this,
@@ -162,17 +177,16 @@ void RefillUnblindedTokens::OnRequestSignedTokens(
   }
   nonce_ = *nonce;
 
-  // Get signed tokens
   GetSignedTokens();
 }
 
 void RefillUnblindedTokens::GetSignedTokens() {
   BLOG(1, "GetSignedTokens");
-  BLOG(2, "GET /v1/confirmation/token/{payment_id}?nonce={nonce}");
+  BLOG(2, "GET /v2/confirmation/token/{payment_id}?nonce={nonce}");
 
   GetSignedTokensUrlRequestBuilder url_request_builder(wallet_, nonce_);
   mojom::UrlRequestPtr url_request = url_request_builder.Build();
-  BLOG(5, UrlRequestToString(url_request));
+  BLOG(6, UrlRequestToString(url_request));
   BLOG(7, UrlRequestHeadersToString(url_request));
 
   auto callback = std::bind(&RefillUnblindedTokens::OnGetSignedTokens, this,
@@ -183,6 +197,11 @@ void RefillUnblindedTokens::GetSignedTokens() {
 void RefillUnblindedTokens::OnGetSignedTokens(
     const mojom::UrlResponse& url_response) {
   BLOG(1, "OnGetSignedTokens");
+
+  if (!get_issuers_->IsInitialized()) {
+    OnFailedToRefillUnblindedTokens(/* should_retry */ false);
+    return;
+  }
 
   BLOG(6, UrlResponseToString(url_response));
   BLOG(7, UrlResponseHeadersToString(url_response));
@@ -218,11 +237,10 @@ void RefillUnblindedTokens::OnGetSignedTokens(
   }
 
   // Validate public key
-  if (*public_key_base64 != public_key_) {
+  if (!get_issuers_->PublicKeyExists("confirmations", *public_key_base64)) {
     BLOG(0, "Response public key " << *public_key_base64
-                                   << " does not match "
-                                      "catalog issuers public key "
-                                   << public_key_);
+                                   << " does not match any"
+                                   << " confirmation public key");
     OnFailedToRefillUnblindedTokens(/* should_retry */ false);
     return;
   }
@@ -275,7 +293,7 @@ void RefillUnblindedTokens::OnGetSignedTokens(
   if (privacy::ExceptionOccurred()) {
     BLOG(1, "Failed to verify and unblind tokens");
     BLOG(1, "  Batch proof: " << *batch_proof_base64);
-    BLOG(1, "  Public key: " << public_key_);
+    BLOG(1, "  Public key: " << public_key.encode_base64());
 
     OnFailedToRefillUnblindedTokens(/* should_retry */ false);
     return;
@@ -288,7 +306,9 @@ void RefillUnblindedTokens::OnGetSignedTokens(
     privacy::UnblindedTokenInfo unblinded_token;
     unblinded_token.value = batch_dleq_proof_unblinded_token;
     unblinded_token.public_key = public_key;
-
+    // TODO: Check if these values must be defined
+    unblinded_token.confirmation_type = ConfirmationType::kUndefined;
+    unblinded_token.ad_type = AdType::kUndefined;
     unblinded_tokens.push_back(unblinded_token);
   }
 

vendor/bat-native-ads/src/bat/ads/internal/tokens/refill_unblinded_tokens/refill_unblinded_tokens.h
@@ -12,6 +12,7 @@
 #include "bat/ads/internal/account/wallet/wallet_info.h"
 #include "bat/ads/internal/backoff_timer.h"
 #include "bat/ads/internal/privacy/tokens/token_generator_interface.h"
+#include "bat/ads/internal/tokens/get_issuers/get_issuers.h"
 #include "bat/ads/internal/tokens/refill_unblinded_tokens/refill_unblinded_tokens_delegate.h"
 #include "bat/ads/public/interfaces/ads.mojom.h"
 #include "wrapper.hpp"
@@ -34,15 +35,15 @@ class RefillUnblindedTokens {
 
  private:
   WalletInfo wallet_;
-
-  std::string public_key_;
-
   std::string nonce_;
 
   std::vector<Token> tokens_;
   std::vector<BlindedToken> blinded_tokens_;
 
   void Refill();
+
+  void RequestIssuers();
+  void OnRequestIssuers();
 
   void RequestSignedTokens();
   void OnRequestSignedTokens(const mojom::UrlResponse& url_response);
@@ -63,6 +64,8 @@ class RefillUnblindedTokens {
   int CalculateAmountOfTokensToRefill() const;
 
   bool is_processing_ = false;
+
+  std::unique_ptr<GetIssuers> get_issuers_;
 
   privacy::TokenGeneratorInterface* token_generator_;  // NOT OWNED
 

terrorizer1980/brave-core

vendor/bat-native-ads/src/bat/ads/internal/tokens/issuers/get_issuers_url_request_builder.cc
@@ -1,0 +1,34 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "bat/ads/internal/tokens/issuers/get_issuers_url_request_builder.h"
+
+#include "base/strings/stringprintf.h"
+#include "bat/ads/internal/server/confirmations_server_util.h"
+
+namespace ads {
+
+GetIssuersUrlRequestBuilder::GetIssuersUrlRequestBuilder() = default;
+
+GetIssuersUrlRequestBuilder::~GetIssuersUrlRequestBuilder() = default;
+
+// GET /v1/issuers/
+
+mojom::UrlRequestPtr GetIssuersUrlRequestBuilder::Build() {
+  mojom::UrlRequestPtr url_request = mojom::UrlRequest::New();
+  url_request->url = BuildUrl();
+  url_request->method = mojom::UrlRequestMethod::kGet;
+
+  return url_request;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+std::string GetIssuersUrlRequestBuilder::BuildUrl() const {
+  return base::StringPrintf("%s/v1/issuers/",
+                            confirmations::server::GetHost().c_str());
+}
+
+}  // namespace ads

vendor/bat-native-ads/src/bat/ads/internal/tokens/redeem_unblinded_token/redeem_unblinded_token.h
@@ -6,8 +6,7 @@
 #ifndef BRAVE_VENDOR_BAT_NATIVE_ADS_SRC_BAT_ADS_INTERNAL_TOKENS_REDEEM_UNBLINDED_TOKEN_REDEEM_UNBLINDED_TOKEN_H_
 #define BRAVE_VENDOR_BAT_NATIVE_ADS_SRC_BAT_ADS_INTERNAL_TOKENS_REDEEM_UNBLINDED_TOKEN_REDEEM_UNBLINDED_TOKEN_H_
 
-#include <string>
-
+#include "bat/ads/internal/tokens/issuers/issuers.h"
 #include "bat/ads/internal/tokens/redeem_unblinded_token/redeem_unblinded_token_delegate.h"
 #include "bat/ads/public/interfaces/ads.mojom.h"
 
@@ -35,6 +34,9 @@ class RedeemUnblindedToken {
   void OnCreateConfirmation(const mojom::UrlResponse& url_response,
                             const ConfirmationInfo& confirmation);
 
+  void RequestIssuers(const ConfirmationInfo& confirmation);
+  void OnRequestIssuers(const ConfirmationInfo& confirmation);
+
   void FetchPaymentToken(const ConfirmationInfo& confirmation);
   void OnFetchPaymentToken(const mojom::UrlResponse& url_response,
                            const ConfirmationInfo& confirmation);
@@ -46,6 +48,7 @@ class RedeemUnblindedToken {
   void OnFailedToRedeemUnblindedToken(const ConfirmationInfo& confirmation,
                                       const bool should_retry);
 
+  std::unique_ptr<Issuers> issuers_;
   RedeemUnblindedTokenDelegate* delegate_ = nullptr;
 };
 

vendor/bat-native-ads/src/bat/ads/internal/tokens/refill_unblinded_tokens/refill_unblinded_tokens.cc
@@ -21,6 +21,7 @@
 #include "bat/ads/internal/privacy/unblinded_tokens/unblinded_tokens.h"
 #include "bat/ads/internal/server/ads_server_util.h"
 #include "bat/ads/internal/time_formatting_util.h"
+#include "bat/ads/internal/tokens/issuers/get_issuers_url_request_builder.h"
 #include "bat/ads/internal/tokens/refill_unblinded_tokens/get_signed_tokens_url_request_builder.h"
 #include "bat/ads/internal/tokens/refill_unblinded_tokens/request_signed_tokens_url_request_builder.h"
 #include "net/http/http_status_code.h"
@@ -42,9 +43,11 @@ const int kMaximumUnblindedTokens = 50;
 
 }  // namespace
 
+// TODO(tmancey): Why do we inject Issuers as issuers are a separate concern and
+// should be fetched and persisted
 RefillUnblindedTokens::RefillUnblindedTokens(
     privacy::TokenGeneratorInterface* token_generator)
-    : token_generator_(token_generator) {
+    : issuers_(std::make_unique<Issuers>()), token_generator_(token_generator) {
   DCHECK(token_generator_);
 }
 
@@ -80,19 +83,18 @@ void RefillUnblindedTokens::MaybeRefill(const WalletInfo& wallet) {
 
   wallet_ = wallet;
 
-  const CatalogIssuersInfo catalog_issuers =
-      ConfirmationsState::Get()->get_catalog_issuers();
-  if (!catalog_issuers.IsValid()) {
-    BLOG(0, "Failed to refill unblinded tokens due to missing catalog issuers");
+  // const CatalogIssuersInfo catalog_issuers =
+  //     ConfirmationsState::Get()->get_catalog_issuers();
+  // if (!catalog_issuers.IsValid()) {
+  //   BLOG(0, "Failed to refill unblinded tokens due to missing catalog
+  //   issuers");
 
-    if (delegate_) {
-      delegate_->OnFailedToRefillUnblindedTokens();
-    }
+  //   if (delegate_) {
+  //     delegate_->OnFailedToRefillUnblindedTokens();
+  //   }
 
-    return;
-  }
-
-  public_key_ = catalog_issuers.public_key;
+  //   return;
+  // }
 
   Refill();
 }
@@ -108,12 +110,26 @@ void RefillUnblindedTokens::Refill() {
 
   nonce_ = "";
 
+  RequestIssuers();
+}
+
+void RefillUnblindedTokens::RequestIssuers() {
+  auto callback = std::bind(&RefillUnblindedTokens::OnRequestIssuers, this);
+  issuers_->GetIssuers(callback);
+}
+
+void RefillUnblindedTokens::OnRequestIssuers() {
+  if (!issuers_->IsInitialized()) {
+    OnFailedToRefillUnblindedTokens(/* should_retry */ false);
+    return;
+  }
+
   RequestSignedTokens();
 }
 
 void RefillUnblindedTokens::RequestSignedTokens() {
   BLOG(1, "RequestSignedTokens");
-  BLOG(2, "POST /v1/confirmation/token/{payment_id}");
+  BLOG(2, "POST /v2/confirmation/token/{payment_id}");
 
   const int count = CalculateAmountOfTokensToRefill();
   tokens_ = token_generator_->Generate(count);
@@ -123,7 +139,7 @@ void RefillUnblindedTokens::RequestSignedTokens() {
   RequestSignedTokensUrlRequestBuilder url_request_builder(wallet_,
                                                            blinded_tokens_);
   mojom::UrlRequestPtr url_request = url_request_builder.Build();
-  BLOG(5, UrlRequestToString(url_request));
+  BLOG(6, UrlRequestToString(url_request));
   BLOG(7, UrlRequestHeadersToString(url_request));
 
   auto callback = std::bind(&RefillUnblindedTokens::OnRequestSignedTokens, this,
@@ -162,17 +178,16 @@ void RefillUnblindedTokens::OnRequestSignedTokens(
   }
   nonce_ = *nonce;
 
-  // Get signed tokens
   GetSignedTokens();
 }
 
 void RefillUnblindedTokens::GetSignedTokens() {
   BLOG(1, "GetSignedTokens");
-  BLOG(2, "GET /v1/confirmation/token/{payment_id}?nonce={nonce}");
+  BLOG(2, "GET /v2/confirmation/token/{payment_id}?nonce={nonce}");
 
   GetSignedTokensUrlRequestBuilder url_request_builder(wallet_, nonce_);
   mojom::UrlRequestPtr url_request = url_request_builder.Build();
-  BLOG(5, UrlRequestToString(url_request));
+  BLOG(6, UrlRequestToString(url_request));
   BLOG(7, UrlRequestHeadersToString(url_request));
 
   auto callback = std::bind(&RefillUnblindedTokens::OnGetSignedTokens, this,
@@ -183,6 +198,11 @@ void RefillUnblindedTokens::GetSignedTokens() {
 void RefillUnblindedTokens::OnGetSignedTokens(
     const mojom::UrlResponse& url_response) {
   BLOG(1, "OnGetSignedTokens");
+
+  if (!issuers_->IsInitialized()) {
+    OnFailedToRefillUnblindedTokens(/* should_retry */ false);
+    return;
+  }
 
   BLOG(6, UrlResponseToString(url_response));
   BLOG(7, UrlResponseHeadersToString(url_response));
@@ -218,11 +238,10 @@ void RefillUnblindedTokens::OnGetSignedTokens(
   }
 
   // Validate public key
-  if (*public_key_base64 != public_key_) {
+  if (!issuers_->PublicKeyExists("confirmations", *public_key_base64)) {
     BLOG(0, "Response public key " << *public_key_base64
-                                   << " does not match "
-                                      "catalog issuers public key "
-                                   << public_key_);
+                                   << " does not match any"
+                                   << " confirmation public key");
     OnFailedToRefillUnblindedTokens(/* should_retry */ false);
     return;
   }
@@ -275,7 +294,7 @@ void RefillUnblindedTokens::OnGetSignedTokens(
   if (privacy::ExceptionOccurred()) {
     BLOG(1, "Failed to verify and unblind tokens");
     BLOG(1, "  Batch proof: " << *batch_proof_base64);
-    BLOG(1, "  Public key: " << public_key_);
+    BLOG(1, "  Public key: " << public_key.encode_base64());
 
     OnFailedToRefillUnblindedTokens(/* should_retry */ false);
     return;
@@ -288,7 +307,9 @@ void RefillUnblindedTokens::OnGetSignedTokens(
     privacy::UnblindedTokenInfo unblinded_token;
     unblinded_token.value = batch_dleq_proof_unblinded_token;
     unblinded_token.public_key = public_key;
-
+    // TODO: Check if these values must be defined
+    unblinded_token.confirmation_type = ConfirmationType::kUndefined;
+    unblinded_token.ad_type = AdType::kUndefined;
     unblinded_tokens.push_back(unblinded_token);
   }
 

vendor/bat-native-ads/src/bat/ads/internal/tokens/refill_unblinded_tokens/refill_unblinded_tokens.h
@@ -12,6 +12,7 @@
 #include "bat/ads/internal/account/wallet/wallet_info.h"
 #include "bat/ads/internal/backoff_timer.h"
 #include "bat/ads/internal/privacy/tokens/token_generator_interface.h"
+#include "bat/ads/internal/tokens/issuers/issuers.h"
 #include "bat/ads/internal/tokens/refill_unblinded_tokens/refill_unblinded_tokens_delegate.h"
 #include "bat/ads/public/interfaces/ads.mojom.h"
 #include "wrapper.hpp"
@@ -34,15 +35,15 @@ class RefillUnblindedTokens {
 
  private:
   WalletInfo wallet_;
-
-  std::string public_key_;
-
   std::string nonce_;
 
   std::vector<Token> tokens_;
   std::vector<BlindedToken> blinded_tokens_;
 
   void Refill();
+
+  void RequestIssuers();
+  void OnRequestIssuers();
 
   void RequestSignedTokens();
   void OnRequestSignedTokens(const mojom::UrlResponse& url_response);
@@ -63,6 +64,8 @@ class RefillUnblindedTokens {
   int CalculateAmountOfTokensToRefill() const;
 
   bool is_processing_ = false;
+
+  std::unique_ptr<Issuers> issuers_;
 
   privacy::TokenGeneratorInterface* token_generator_;  // NOT OWNED
 